42b72bac99246ef3abfd2bb5ab255abfd077bc3b
sidhant-sriv <sidhant.sriv@gmail.com>
2021-07-06 23:03:53 +0530

    2 new features(mems and jokes)

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,13 @@
 import discord
+import os
 from discord.ext import commands
 import random
+import praw
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv('../.env'))
+CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
+CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+USER_AGENT = os.environ['REDDIT_USER_AGENT']
 
 
 class Fun(commands.Cog):
@@ -30,6 +37,34 @@ class Fun(commands.Cog):
                      "Outlook not so good.",
                      "Very doubtful."]
         await ctx.send(f'Question: {question} \n Answer: {random.choice(responses)}')
+
+    @commands.command()
+    async def meme(self, ctx):
+        reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                             user_agent=USER_AGENT, check_for_async=False)
+        post = reddit.subreddit(
+            'memes+dankmemes+me_irl+MadeMeSmile+cursedcomments').hot(limit=20)
+        post = [i for i in post]
+        post = random.choice(post)
+        res = discord.Embed(
+            title=f'**{post.title}**', url=f'https://reddit.com{post.permalink}', colour=discord.Colour(0x000000))
+        res.set_footer(text=f'{post.score} 👍')
+        res.set_image(url=post.url)
+        await ctx.send(embed=res)
+
+    @commands.command()
+    async def joke(self, ctx):
+
+        reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                             user_agent=USER_AGENT, check_for_async=False)
+
+        post = reddit.subreddit('Jokes+darkjokes').hot(limit=30)
+        post = [i for i in post]
+        post = random.choice(post)
+
+        res = discord.Embed(
+            title=f'**{post.title}**', url=f'https://reddit.com{post.permalink}', description=post.selftext, colour=discord.Colour(0x000000))
+        await ctx.send(embed=res)
 
 
 def setup(client):
