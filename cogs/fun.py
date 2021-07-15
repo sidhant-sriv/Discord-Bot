3826f36fcd9b8d3bd87039d0bc6074aea500716a
sidhant-sriv <sidhant.sriv@gmail.com>
2021-07-15 08:58:54 +0530

    imgur image search

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import imgurpython
+import requests
 from discord.ext import commands
 import random
 import praw
@@ -9,7 +11,8 @@ load_dotenv(find_dotenv('../.env'))
 CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
 CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
 USER_AGENT = os.environ['REDDIT_USER_AGENT']
-
+IMGUR_ID = os.environ['IMGUR_CLIENT_ID']
+IMGUR_SECRET = os.environ['IMGUR_CLIENT_SECRET']
 
 class Fun(commands.Cog):
     """Stuff that's fun"""
@@ -79,7 +82,16 @@ class Fun(commands.Cog):
             res = discord.Embed(title=f'{sentence}', description= f'Calculated sentiment {words.sentiment.polarity}')
             res.set_footer(text="If value>0 then positive, <0 then negative and =0 then neutral sentiment")
             await ctx.send(embed=res)
-
+    @commands.command()
+    async def imgur(self, ctx, *, query):
+        """Returns an image from imgur"""
+        if query:
+            client = imgurpython.ImgurClient(IMGUR_ID,IMGUR_SECRET)
+            items = client.gallery_search(query)[:]
+            post = random.choice(items)
+            res = discord.Embed(title=post.title)
+            res.set_image(url=post.link)
+            await ctx.send(post.link)
 
 def setup(client):
     client.add_cog(Fun(client))
