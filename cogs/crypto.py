396dc9115dc7fce14feb809660907d20caaaa9ae
sidhant-sriv <sidhant.sriv@gmail.com>
2022-01-27 18:44:38 +0530

    more fixes

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -6,8 +6,29 @@
 from email import utils
 import discord
 from discord.ext import commands
-from ..utils import CaeserCipher
+class CaeserCipher:
+    
+    def encrypt(self, text,key):
+        result=""
+        for i in text:
+            if i.isupper():
+                result+=chr((ord(i)+key-65)%26+65)
+            elif i.islower():
+                result+=chr((ord(i)+key-97)%26+97)
+            else:
+                result+=i
+        return result
 
+    def decrypt(self,text,key):
+        result=""
+        for i in text:
+            if i.isupper():
+                result+=chr((ord(i)-key-65)%26+65)
+            elif i.islower():
+                result+=chr((ord(i)-key-97)%26+97)
+            else:
+                result+=i
+        return result
 
 class Crypto(commands.Cog):
     def __init__(self, client):
