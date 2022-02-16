cdb8af1f98894f4bb38691077b25c6f561cdb945
sidhant-sriv <sidhant.sriv@gmail.com>
2022-02-16 09:41:58 +0530

    poll redo

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,94 +1,65 @@
-import re
+import discord
 from discord.ext import commands
 
 
-class Poll(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-        self.polls = {}
-        self.emoji = {
-            "0": "0\u20e3",
-            "1": "1\u20e3",
-            "2": "2\u20e3",
-            "3": "3\u20e3",
-            "4": "4\u20e3",
-            "5": "5\u20e3",
-            "6": "6\u20e3",
-            "7": "7\u20e3",
-            "8": "8\u20e3",
-            "9": "9\u20e3",
-            "a": "🇦",
-            "b": "🇧",
-            "c": "🇨",
-            "d": "🇩",
-            "e": "🇪",
-            "f": "🇫",
-            "g": "🇬",
-            "h": "🇭",
-            "i": "🇮",
-            "j": "🇯",
-            "k": "🇰",
-            "l": "🇱",
-            "m": "🇲",
-            "n": "🇳",
-            "o": "🇴",
-            "p": "🇵",
-            "q": "🇶",
-            "r": "🇷",
-            "s": "🇸",
-            "t": "🇹",
-            "u": "🇺",
-            "v": "🇻",
-            "w": "🇼",
-            "x": "🇽",
-            "y": "🇾",
-            "z": "🇿"
-        }
+class Poll:
+    """"""
 
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        msg = reaction.message
-        message_id = str(msg.id)
-        if user.bot:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def poll(self, ctx, question, *options: str):
+        if len(options) <= 1:
+            await self.bot.say('You need more than one option to make a poll!')
             return
-        if message_id in self.polls:
-            if reaction.emoji not in self.polls[message_id]:
-                await msg.remove_reaction(reaction, user)
-            else:
-                for r in msg.reactions:
-                    async for u in r.users():
-                        if not u.id == user.id:
-                            continue
-                        if r.emoji == reaction.emoji:
-                            continue
-                        await msg.remove_reaction(r, user)
+        if len(options) > 10:
+            await self.bot.say('You cannot make a poll for more than 10 things!')
+            return
 
+        if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+            reactions = ['✅', '❌']
+        else:
+            reactions = [':one:', ':two:', ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine", ":ten:"]
 
-    @commands.command(
-        name='poll',
-        hidden=True,
-    )
-    async def make_poll(self, ctx, *, poll_string):
-        """Create a Poll
-        Example use:
-        ```
-        felix poll
-        Question
-        0. Possibility0
-        1: Possibility1
-        a. Possibility2
-        b) Possibility3
-        ```"""
-        re_find = re.findall(
-            r'^([0-9a-zA-Z])(?:\.|\:|\))\s', poll_string, flags=re.M
-        )
-        choices = [r for r in re_find]
-        poll_msg = ctx.message
-        poll_id = str(poll_msg.id)
-        for choice in choices:
-            react_emoji = self.emoji[choice.lower()]
-            self.polls[poll_id] = self.polls.get(poll_id, []) + [react_emoji]
-            await poll_msg.add_reaction(react_emoji)
+        description = []
+        for x, option in enumerate(options):
+            description += '\n {} {}'.format(reactions[x], option)
+        embed = discord.Embed(title=question, description=''.join(description))
+        react_message = await self.bot.say(embed=embed)
+        for reaction in reactions[:len(options)]:
+            await self.bot.add_reaction(react_message, reaction)
+        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+        await self.bot.edit_message(react_message, embed=embed)
+
+    @commands.command(pass_context=True)
+    async def tally(self, ctx, id):
+        poll_message = await self.bot.get_message(ctx.message.channel, id)
+        if not poll_message.embeds:
+            return
+        embed = poll_message.embeds[0]
+        if poll_message.author != ctx.message.server.me:
+            return
+        if not embed['footer']['text'].startswith('Poll ID:'):
+            return
+        unformatted_options = [x.strip() for x in embed['description'].split('\n')]
+        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
+            else {x[:1]: x[2:] for x in unformatted_options}
+        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
+        voters = [ctx.message.server.me.id]  # add the bot's ID to the list of voters to exclude it's votes
+
+        tally = {x: 0 for x in opt_dict.keys()}
+        for reaction in poll_message.reactions:
+            if reaction.emoji in opt_dict.keys():
+                reactors = await self.bot.get_reaction_users(reaction)
+                for reactor in reactors:
+                    if reactor.id not in voters:
+                        tally[reaction.emoji] += 1
+                        voters.append(reactor.id)
+
+        output = 'Results of the poll for "{}":\n'.format(embed['title']) + \
+                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
+        await self.bot.say(output)
 
 
 def setup(bot):
